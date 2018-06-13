d9f61c95a5ed42770d1c3d5958a623e71fd4b8c9
Carlos Arias <carlos.23325@gmail.com>
2018-06-12 20:42:11 -0500

    add event binding when a button is clicked

--- day-one/day-one-task/src/app/app.component.ts
+++ day-one/day-one-task/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-	title = 'my application';
-	author = 'Carlos Andres Arias Londo#o';
+	title: string = 'my application';
+	author: string = 'Carlos Andres Arias Londo#o';
+	count: number = 0;
 }

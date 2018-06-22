696e642874c4cad1dbe1e51060e7c709e21f62e3
Carlos Arias <carlos.23325@gmail.com>
2018-06-22 15:02:37 -0500

    add employee function

--- project/src/app/data-manager.service.ts
+++ project/src/app/data-manager.service.ts
@@ -8,9 +8,16 @@ import { Employee } from './employee.interface';
 })
 export class DataManagerService {
 	urlBase: string = 'api/';
+	httpOptions = {
+		headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+	};
 	constructor(private http: HttpClient) {}
 
 	getData<T>(url: string): Observable<T> {
 		return this.http.get<T>(`${this.urlBase}${url}`);
+	}
+
+	addData<T>(employee: Employee, url: string): Observable<T> {
+		return this.http.post<T>(`${this.urlBase}${url}`, employee, this.httpOptions);
 	}
 }
--- project/src/app/employees/employees.component.ts
+++ project/src/app/employees/employees.component.ts
@@ -1,3 +1,4 @@
+import { Project } from './../project.interface';
 import { Employee } from './../employee.interface';
 import { DataManagerService } from './../data-manager.service';
 import { Component, OnInit } from '@angular/core';
@@ -13,5 +14,18 @@ export class EmployeesComponent implements OnInit {
 
 	ngOnInit() {
 		this.dataManagerService.getData<Employee[]>('employees').subscribe((data) => (this.employees = data));
+	}
+	add(name: string, company: string, age: string, birthday: string, favcolor: string, project: number) {
+		const data = {
+			name: name,
+			company: company,
+			age: age,
+			birthday: birthday,
+			favcolor: favcolor,
+			project: project
+		};
+		this.dataManagerService.addData<Employee>(data as Employee, 'employees').subscribe((employee) => {
+			this.employees.push(employee);
+		});
 	}
 }
